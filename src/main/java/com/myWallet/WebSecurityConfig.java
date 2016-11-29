0a83b01de4eb0914b7e5cd1b147077f40e987d6e
1nevi <ziniewicz.p@gmail.com>
2016-11-29 22:56:11 +0100

    add CORS Filter

--- src/main/java/com/myWallet/WebSecurityConfig.java
+++ src/main/java/com/myWallet/WebSecurityConfig.java
@@ -5,17 +5,20 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.access.channel.ChannelProcessingFilter;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 
 @Configuration
 @EnableWebSecurity
 public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
+	
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+        	.addFilterBefore(new CorsFilter(), ChannelProcessingFilter.class)
         	.csrf().disable()
             .authorizeRequests()
-                .antMatchers("/", "/index", "/register").permitAll()
+                .antMatchers("/", "/index").permitAll()
                 .anyRequest().authenticated()
                 .and()
             .formLogin()
@@ -25,11 +28,11 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
             .logout()
                 .permitAll();
     }
-
+    
     @Autowired
     public void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
-        auth
-            .inMemoryAuthentication()
-                .withUser("user").password("pw").roles("USER");
+        auth.inMemoryAuthentication().withUser("zbigniew").password("haslo").roles("USER");
+        auth.inMemoryAuthentication().withUser("piotr").password("haslo").roles("USER");
+        auth.inMemoryAuthentication().withUser("user").password("pw").roles("USER");
     }
 }
